0844805cc682fed7830f54c8003e2b2c6bd70067
hsu <hang.su1987@gmail.com>
2013-12-11 17:45:51 -0500

    update note.txt study until q9

--- src/com/exam/Deeper_Q9.java
+++ src/com/exam/Deeper_Q9.java
@@ -1,17 +1,20 @@
 package com.exam;
 
-public class Deeper_Q9 {
+public class Deeper_Q9 extends Q9{
 
-	// protected Integer getDepth() {
-	// return 5;
-	// }
-
+//	protected Integer getDepth() {
+//		return 5;
+//	 }
+	
+	public Short getDepth(){
+		return 5;
+	}
 	public Long getDepth(int d) {
 		return 5L;
 	}
 
 	public static void main(String[] args) {
 		Deeper_Q9 deeper = new Deeper_Q9();
-		System.out.println(deeper.getDepth(2));
+		System.out.println(deeper.getDepth());
 	}
 }
