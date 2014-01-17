06512cf8f7577a325cb8c0c21a0455d0686867bf
Hang Su <hang.su1987@gmail.com>
2014-01-17 12:17:51 -0500

    Update note.txt Add q132

---
+++ src/com/exam/Q132.java
@@ -0,0 +1,12 @@
+package com.exam;
+
+public class Q132 extends Q132_Boat {
+
+	public static void main(String[] args) {
+		Q132_Boat b = new Q132(); // Line A
+		//Q132_Boat b2 = new Q132_Boat(); // Line B
+		}
+		String doFloat() { return "slow float"; } // Line C
+		void doDock() { } // Line D
+
+}
---
+++ src/com/exam/Q132_Boat.java
@@ -0,0 +1,9 @@
+package com.exam;
+
+public abstract class Q132_Boat {
+	String doFloat() {
+		return "floating";
+	}
+
+	abstract void doDock();
+}
